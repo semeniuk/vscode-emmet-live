61050798980ad30bca017f09c0300f09a1bd693b
Yurii Semeniuk <mail@ysemeniuk.com>
2019-10-20 18:18:38 +0300

    use `vscode-emmet-helper` instead of `emmet` directly; tabstops support added; undo behavior improved; minor changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## [1.0.0]
+- use `vscode-emmet-helper` instead of `emmet` directly;
+- tabstops support added;
+- undo behavior improved;
+- minor changes;
+
 ## [0.1.2]
 - `Ctrl+K Ctrl+L` keyboard shortcut added
 - minor changes
--- extension.js
+++ extension.js
@@ -1,58 +1,81 @@
 // VS Code API Reference: https://code.visualstudio.com/api/references/vscode-api
 
-var vscode = require('vscode');
-var parser = require('emmet/lib/parser/abbreviation');
+const vscode = require('vscode');
+
+let emmetHelper;
+function getEmmetHelper() {
+    // Lazy load vscode-emmet-helper instead of importing it
+    // directly to reduce the start-up time of the extension
+    if (!emmetHelper) {
+        emmetHelper = require('vscode-emmet-helper');
+    }
+    return emmetHelper;
+}
 
 function expand(abbr) {
-    var result = parser.expand(abbr);
+    let helper = getEmmetHelper(),
+        expandOptions = helper.getExpandOptions('html'), // TODO: consider editor.document.languageId
+        parsedAbbr = helper.parseAbbreviation(abbr);
+    return helper.expandAbbreviation(parsedAbbr, expandOptions);
+}
+
+function expandPreview(abbr) {
+    let result = expand(abbr);
     // replace tabstops with their placeholders (if any)
-    result = result.replace(/\$\{\d+\:?(.*?)\}/g, "$1");
-    return result;
+    return result.replace(/\$\{\d+\:?(.*?)\}/g, '$1');
 }
 
 function activate(context) {
-    var disposable = vscode.commands.registerCommand('ysemeniuk.emmetLive', function () {
+    const disposable = vscode.commands.registerCommand('ysemeniuk.emmetLive', () => {
 
-        var editor = vscode.window.activeTextEditor;
+        const editor = vscode.window.activeTextEditor;
         if (!editor) {
             vscode.window.showInformationMessage('Emmet Live: no active text editor');
             return; // No open text editor
         }
-                
-        var originalText = editor.document.getText(editor.selection);
+
+        let originalText = editor.document.getText(editor.selection);
 
         // if selection is empty then try to select current line
+        // TODO: improve this behavior with emmetHelper.extractAbbreviation and/or emmetHelper.extractAbbreviationFromText
         if (!originalText) {
-            var currentLineIndex = editor.selection.active.line;
-            var currentLine = editor.document.lineAt(currentLineIndex);
+            let currentLineIndex = editor.selection.active.line;
+            let currentLine = editor.document.lineAt(currentLineIndex);
             originalText = currentLine ? currentLine.text : originalText;
 
             // select current line
-            var selectionFrom = new vscode.Position(currentLineIndex, 0);
-            var selectionTo = new vscode.Position(currentLineIndex, originalText.length);
+            let selectionFrom = new vscode.Position(currentLineIndex, 0);
+            let selectionTo = new vscode.Position(currentLineIndex, originalText.length);
             editor.selections = [new vscode.Selection(selectionFrom, selectionTo)];
         }
 
+        let recordUndoStops = true;
+
         vscode.window.showInputBox({
-            placeHolder: "Emmet Abbreviation",
-            prompt: "Type your Emmet abbreviation",
+            placeHolder: 'Emmet Abbreviation',
+            prompt: 'Type your Emmet abbreviation',
             value: originalText.trim(),
             validateInput: function (abbr) {
                 try {
-                    var result = expand(abbr);
+                    let result = expandPreview(abbr);
+                    editor.edit((editBuilder) => {
+                        editBuilder.replace(editor.selection, result);
+                    }, { undoStopAfter: false, undoStopBefore: recordUndoStops });
+                    // record only first edit
+                    recordUndoStops = false;
                 } catch (e) {
-                    return "Invalid Emmet abbreviation";
+                    return 'Invalid Emmet abbreviation';
                 }
-                editor.edit(function (editBuilder) {
-                    editBuilder.replace(editor.selection, result);
-                }, { undoStopAfter: false, undoStopBefore: false });
             }
-        }).then(function (abbr) {
-            // if input dismissed (abbr is undefined) then restore original text
-            if (!abbr) {
-                editor.edit(function(editBuilder){
+        }).then((abbr) => {
+            if (abbr) {
+                let result = expand(abbr);
+                editor.insertSnippet(new vscode.SnippetString(result));
+            } else {
+                // if input dismissed (abbr is undefined) then restore original text
+                editor.edit((editBuilder) => {
                     editBuilder.replace(editor.selection, originalText);
-                }, {undoStopAfter: false, undoStopBefore: false});  
+                }, { undoStopAfter: false, undoStopBefore: false });
             }
         });
 
